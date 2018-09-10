24fe23e8da4a3474bcc7cae5f51256ed1440131a
wblong <sdwangbaolong@163.com>
2018-09-10 17:56:11 +0800

    Receiving messages based on a pattern (topics)

---
+++ _EmitLogTopic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace _EmitLogTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName="localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+
+                var routeKey = args.Length > 0 ? args[0] : "anonymous.info";
+                var message = args.Length > 1 ? String.Join(" ", args.Skip(1).ToArray()) : "Hello World";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "topic_logs", routingKey: routeKey, basicProperties: null, body: body);
+
+                Console.WriteLine("[x] Send {0}:{1}",routeKey,message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ _ReceiveLogsTopic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace _ReceiveLogsTopic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.WriteLine("Args Error :Usage [0] [binding keys....]",Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine("Press [enter] to exit!");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+
+                }
+
+                foreach(var bindingkey in args)
+                {
+                    channel.QueueBind(queue: queueName,exchange:"topic_logs",routingKey: bindingkey);
+
+                }
+                var consume = new EventingBasicConsumer(channel);
+
+                consume.Received += (model, ea) => {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine("[x] send {0} :{1}",routingKey,message);
+
+                };
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consume);
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
